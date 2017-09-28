e46a5f4c3aa3dfd25b105f49a55863506916f45f
Ethan Jaynes <ethandjay@gmail.com>
2017-09-27 20:32:51 -0500

    onclicks are now addEventListener("click",...

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -39,83 +39,89 @@ function showPage(id) {
 // Defualt action of all link buttons is to direct to their name's corresponding page
 var links = document.getElementsByClassName("direct");
 for (let link of links) {
-	link.onclick = function() {
-		showPage(link.name);
+		link.addEventListener("click", function() {
+			showPage(link.name);
+		}
+	)
+}
+
+document.getElementById("signup").addEventListener("click", function() {
+		var username = document.getElementById("newuser").value;
+		var password = document.getElementById("newpass").value;
+		var phone = document.getElementById("newphone").value;
+		var gid = document.getElementById("newID").value;
+		var newUser = new user(username, password, phone, gid);
+		users.push(newUser);
+		currentUser = newUser;
+		showPage("dashboard");
 	}
-}
+)
 
-document.getElementById("signup").onclick = function() {
-	var username = document.getElementById("newuser").value;
-	var password = document.getElementById("newpass").value;
-	var phone = document.getElementById("newphone").value;
-	var gid = document.getElementById("newID").value;
-	var newUser = new user(username, password, phone, gid);
-	users.push(newUser);
-	currentUser = newUser;
-	showPage("dashboard");
-}
+document.getElementById("sendemail").addEventListener("click", function () {
+		// WOW, super clean way to generate random characters, thanks https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+		alert("Your new password is " + Math.random().toString(36).substring(4));
+	}
+)
 
-document.getElementById("sendemail").onclick = function () {
-	// WOW, super clean way to generate random characters, thanks https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-	alert("Your new password is " + Math.random().toString(36).substring(4));
-}
+document.getElementById("dologin").addEventListener("click", function () {
+		var username = document.getElementById("currentuser").value;
+		var password = document.getElementById("currentpass").value;
+		for (let user of users) {
+			if (user.username = username && user.password == password){
+				currentUser = user;
 
-document.getElementById("dologin").onclick = function () {
-	var username = document.getElementById("currentuser").value;
-	var password = document.getElementById("currentpass").value;
-	for (let user of users) {
-		if (user.username = username && user.password == password){
-			currentUser = user;
+				var isOpen = currentUser.status;
+				for (let p of document.getElementsByClassName("status")) {
+					p.style.color = isOpen ? "Green" : "Red";
+					p.innerHTML = isOpen ? "OPEN" : "CLOSED";
+				}
+				document.getElementById("open").disabled = isOpen ? true : false;
+				document.getElementById("close").disabled = isOpen ? false : true;
 
-			var isOpen = currentUser.status;
-			for (let p of document.getElementsByClassName("status")) {
-				p.style.color = isOpen ? "Green" : "Red";
-				p.innerHTML = isOpen ? "OPEN" : "CLOSED";
+				showPage("dashboard");
+				return;
 			}
-			document.getElementById("open").disabled = isOpen ? true : false;
-			document.getElementById("close").disabled = isOpen ? false : true;
-
-			showPage("dashboard");
-			return;
 		}
+		alert("Either your username or password is incorrect");
 	}
-	alert("Either your username or password is incorrect");
-}
+)
 
-document.getElementById("open").onclick = function () {
-	if (!currentUser.status) {
-		for (let p of document.getElementsByClassName("status")) {
-			p.style.color = "Orange";
-			p.innerHTML = "Opening...";
-			document.getElementById("open").disabled = true;
-			document.getElementById("close").disabled = false;
-			setTimeout(function () {
-				p.style.color = "Green";
-				p.innerHTML = "Open";
-			}, 5000);
+document.getElementById("open").addEventListener("click", function () {
+		if (!currentUser.status) {
+			for (let p of document.getElementsByClassName("status")) {
+				p.style.color = "Orange";
+				p.innerHTML = "Opening...";
+				document.getElementById("open").disabled = true;
+				document.getElementById("close").disabled = false;
+				setTimeout(function () {
+					p.style.color = "Green";
+					p.innerHTML = "Open";
+				}, 5000);
+			}
+
+			currentUser.status = !currentUser.status;
 		}
 
-		currentUser.status = !currentUser.status;
 	}
+)
 
-}
 
-
-document.getElementById("close").onclick = function () {
-	if (currentUser.status) {
-		for (let p of document.getElementsByClassName("status")) {
-			p.style.color = "Orange";
-			p.innerHTML = "Closing...";
-			document.getElementById("open").disabled = false;
-			document.getElementById("close").disabled = true;
-			setTimeout(function () {
-				p.style.color = "Red";
-				p.innerHTML = "Closed";
-			}, 5000);
+document.getElementById("close").addEventListener("click",function () {
+		if (currentUser.status) {
+			for (let p of document.getElementsByClassName("status")) {
+				p.style.color = "Orange";
+				p.innerHTML = "Closing...";
+				document.getElementById("open").disabled = false;
+				document.getElementById("close").disabled = true;
+				setTimeout(function () {
+					p.style.color = "Red";
+					p.innerHTML = "Closed";
+				}, 5000);
+			}
+			currentUser.status = !currentUser.status;
 		}
-		currentUser.status = !currentUser.status;
 	}
-}
+)
 
 // Default view
 showPage("login");
